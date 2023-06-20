6120bbda022d8e43c69b6bdad51bbe869257914e
Mannkumar_K._Pandya <75202120+Mannkp@users.noreply.github.com>
2023-06-20 23:19:34 +0530

    logic and UI change
    logic has been updated to provide weather information for Locations in India

--- script.js
+++ script.js
@@ -1,73 +1,63 @@
 let city;
-let input = document.querySelector("#inp");
+let userCityInput = document.querySelector("#inp");
+let searchBtn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 
 //This is a new api and may produce in accurate information, so using simple way around using city names.
-const cityLocation = async (city) => {
+const fetchCityLocation = async (city) => {
     try{
-        console.log("received city: " , city);
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=e5738002c5bea4b0bc71830ac421fe8a`);
-        console.log("response: " , response);
+        // const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=e5738002c5bea4b0bc71830ac421fe8a`);
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},IN&appid=e5738002c5bea4b0bc71830ac421fe8a`);
         const data = await response.json();
-        console.log("data: " , data);
-        latitude = data[0].lat;
-        longitude = data[0].lon;
-        return {latitude, longitude};
+        const { lat, lon } = data[0]; //same as: latitude = data[0].lat; longitude = data[0].lon;
+        return {latitude: lat, longitude: lon};
     }
     catch (error){
         console.log("Error: " , error);
     }
-}
+};
 
-const currentWeather = async (latitude , longitude) => {
+const fetchCurrentWeather = async (latitude , longitude) => {
     try{
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e5738002c5bea4b0bc71830ac421fe8a`);
-
         const data = await res.json();
-        
-        console.log(data);
-
         let desc = data.weather[0].description;
         let icon = data.weather[0].icon;
-        let avgTemp = (data.main.temp) - 273.15;
-        let feelsLike = (data.main.feels_like) - 273.15;
-        let humidity = data.main.humidity;
-        let windSpeed = data.wind.speed;
-        console.log(desc);
-        console.log(avgTemp);
-        console.log(feelsLike);
-        console.log(windSpeed);
-        console.log(humidity);
-        container.innerHTML = `<h1>${city}</h1>`;
-        container.innerHTML += `<h2>Avg. Temperature: ${avgTemp.toPrecision(4)}°C</h2>`;
-        container.innerHTML += `<img src="https://openweathermap.org/img/wn/${icon}.png" class="myIcon"/>`;
-        container.innerHTML += `<h2>Feels Like: ${feelsLike.toPrecision(4)}°C</h2>`;
-        container.innerHTML += `<h2>${desc}</h2>`;
-        container.innerHTML += `<h2>Wind Speed: ${windSpeed.toPrecision(4)} m/s</h2>`;
-        container.innerHTML += `<h2>Humidity: : ${humidity.toPrecision(4)}%</h2>`;
+        let avgTemp = ((data.main.temp) - 273.15).toPrecision(4);
+        let feelsLike = ((data.main.feels_like) - 273.15).toPrecision(4);
+        let humidity = (data.main.humidity).toPrecision(4);
+        let windSpeed = (data.wind.speed).toPrecision(4);
+        container.innerHTML = `<h1>${city}</h1>
+        <h2>Avg. Temperature: ${avgTemp}°C</h2>
+        <img src="https://openweathermap.org/img/wn/${icon}.png" class="myIcon"/>
+        <h2>Feels Like: ${feelsLike}°C</h2>
+        <h2>${desc}</h2>
+        <h2>Wind Speed: ${windSpeed} m/s</h2>
+        <h2>Humidity: ${humidity}%</h2>`;
     }
-    catch(err){
-        console.log("Error: " , err);
+    catch(error){ 
+        console.log("Error: " , error);
     }
 };
 
-let searchBtn = document.querySelector(".btn");
+const searchCity = async () => {
+    city = await userCityInput.value;
+    let {latitude, longitude} = await fetchCityLocation(city);
+    fetchCurrentWeather(latitude,longitude);
+};
+
+
 searchBtn.addEventListener("click" , async () => {
-    city = await input.value;
-    console.log(city);
-    let {latitude, longitude} = await cityLocation(city);
-    console.log("mklat = " , latitude);
-    console.log("mklon = " , longitude);
-    currentWeather(latitude,longitude);
+    searchCity();
 });
 
-input.addEventListener("keydown" , async (e) => {
-    if (e.code == "Enter") {  //checks whether the pressed key is "Enter"
-        city = await input.value;
-        console.log(city);
-        let {latitude, longitude} = await cityLocation(city);
-        console.log("mklat = " , latitude);
-        console.log("mklon = " , longitude);
-        currentWeather(latitude,longitude);
+userCityInput.addEventListener("keydown" , async (e) => {
+    if (e.key == "Enter") {  //checks whether the pressed key is "Enter"
+        searchCity();
     }
 });
+
+if(city === undefined){
+    city="vadodara";
+    fetchCurrentWeather(22.2973142,73.1942567);
+}
